d3d14f8dc12899fb6b65a24506dbe99e01fd7760
dierstee <dierstee@gmail.com>
2020-10-27 21:56:29 +0700

    new completed algorithm case source code add prime factorization case from tlx.toki.id

--- Algorithm Cases/faktorisasi-prima.cpp
+++ Algorithm Cases/faktorisasi-prima.cpp
@@ -1,6 +1,49 @@
-#include <iostream>
 #include <stdio.h>
 
+void primeFactorization(int &x);
+
 int main(){
-	
+	int x;
+	scanf("%d", &x);
+	primeFactorization(x);
 }
+
+void primeFactorization(int &x){
+	int divisor = 2;
+	int power = 0;
+	
+	while(divisor < x){
+		if(x % divisor == 0){
+			int pow = 0;
+			while(x % divisor == 0){
+				x = x/divisor;
+				pow++;
+			}
+			
+			if(pow != 1){
+			   printf("%d^%d", divisor, pow);	
+			} else{
+			   printf("%d", divisor);	
+			}
+			
+			if(x  != 1){
+				printf(" x ");
+			}
+		} else {
+			divisor++;
+		}
+	}
+	if(x != 1){
+		printf("%d", x);	
+	}
+	printf("\n");
+}
+
+
+
+
+
+
+
+
+
